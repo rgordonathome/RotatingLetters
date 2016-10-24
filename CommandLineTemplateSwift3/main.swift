91e451c9df8a0b5b69084a23de887103237e79f6
Russell Gordon <rgordon@rsgc.on.ca>
2016-10-24 16:42:31 -0400

    Initial Commit

---
+++ CommandLineTemplateSwift3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CommandLineTemplateSwift3
+//
+//  Created by Russell Gordon on 10/24/16.
+//  Copyright © 2016 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
